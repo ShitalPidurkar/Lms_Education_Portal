52895e4e0557faaf1d8f27b85408762360543b77
ShitalPidurkar <spidurkar12@gmail.com>
2023-03-30 17:31:09 +0530

    Initial commit

---
+++ shitallms/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from elearn import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('elearn.urls')),
+    path('homepage/', views.homepage, name='homepage'),
+    path('paymenthandler/', views.paymenthandler, name='paymenthandler'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
